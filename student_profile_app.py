3b649803bf2cdad63567d88245d606e8b01909d8
Sohith <sohithpothual@gmail.com>
2026-09-12 23:32:00 +0530

    Created a Basic Student Profile App in Streamlit

---
+++ student_profile_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title("My Student Profile")
+st.header("About Me")
+st.text("""
+Hi hello i am sohith,
+i am studying in 3rd year btech in CSM,
+i am currently 20 years old.
+""")
+st.subheader("My Skills")
+st.write("""
+1. Python
+2. Mechine Learning
+3. Data Science
+4. Full Stack Development""")
+st.markdown("##My goal is to become a strong AI/Data Science Developer")
